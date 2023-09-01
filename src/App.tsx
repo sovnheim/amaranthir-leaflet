cbb85e36fba8277beac9176ed51b96c83fd3834c
sovnheim <felix.machiavelli@gmail.com>
2023-09-01 15:42:32 +0200

    basic map implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
-import { LatLng, Icon } from 'leaflet';
-import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { CRS } from 'leaflet';
 
 function App() {
-	const position = new LatLng(51.505, -0.09);
-
-	//defines behaviour on default icons
-	const DefaultIcon = new Icon({
-		iconUrl: icon,
-		shadowUrl: iconShadow,
-	});
-
 	return (
 		<div className="App">
-			<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+			{/* <img src="/assets/download.jpg" /> */}
+			<MapContainer
+				id="map"
+				center={[0, 0]}
+				zoom={2}
+				minZoom={2}
+				scrollWheelZoom={true}
+				crs={CRS.Simple}
+			>
 				<TileLayer
-					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+					attribution="sovnheim.io"
+					url="/assets/tiles/{z}/{x}/{y}.png"
+					// url="/assets/download.jpg"
 				/>
-				<Marker position={position} icon={DefaultIcon}>
-					<Popup>
-						A pretty CSS3 popup. <br /> Easily customizable.
-					</Popup>
-				</Marker>
 			</MapContainer>
 		</div>
 	);
