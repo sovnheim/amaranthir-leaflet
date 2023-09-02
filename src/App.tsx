c3e56bb475af5cc739b0f3ac25ca086bd9b13345
sovnheim <felix.machiavelli@gmail.com>
2023-09-02 10:41:23 +0200

    fiddling with the CRS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,56 @@
 import './App.css';
 import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import { CRS } from 'leaflet';
+import {
+	MapContainer,
+	Marker,
+	Popup,
+	TileLayer,
+	useMap,
+	useMapEvents,
+} from 'react-leaflet';
+import { CRS, LatLngBounds } from 'leaflet';
+
+function MapClickEvent() {
+	const map = useMap();
+	useMapEvents({
+		click(event) {
+			console.log(
+				'lat',
+				event.latlng.lat,
+				'lng',
+				event.latlng.lng,
+				'zoom',
+				map.getZoom()
+			);
+		},
+	});
+	return <div></div>;
+}
 
 function App() {
+	const bounds = new LatLngBounds([1000, 1000], [0, 0]);
+
 	return (
 		<div className="App">
 			<MapContainer
 				id="map"
 				center={[0, 0]}
 				zoom={2}
-				minZoom={2}
+				minZoom={-5}
+				maxZoom={5}
 				scrollWheelZoom={true}
 				crs={CRS.Simple}
+				bounds={bounds}
 			>
 				<TileLayer
 					attribution="sovnheim.io"
 					url="/assets/tiles/{z}/{x}/{y}.png"
 				/>
+
+				<MapClickEvent />
+				{/* <Marker position={[120, 120]}>
+					<Popup>You are here</Popup>
+				</Marker> */}
 			</MapContainer>
 		</div>
 	);
