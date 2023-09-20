b3cdaa7debb3a0266f46fb877c769a147f7d02be
sovnheim <felix.machiavelli@gmail.com>
2023-09-20 10:36:06 +0200

    merge to main branch

--- src/components/MapClickEvent.tsx
+++ src/components/MapClickEvent.tsx
@@ -1,24 +1,12 @@
-import { useMap, useMapEvents } from 'react-leaflet';
+import { useMapEvents } from 'react-leaflet';
 
 function MapClickEvent() {
-	const map = useMap();
-
 	useMapEvents({
 		click(event) {
 			// eslint-disable-next-line @typescript-eslint/no-floating-promises
 			navigator.clipboard
 				.writeText(`${event.latlng.lat}	${event.latlng.lng}`)
-				.then(() => {
-					// eslint-disable-next-line no-console
-					console.log(
-						'lat',
-						event.latlng.lat,
-						'lng',
-						event.latlng.lng,
-						'zoom',
-						map.getZoom()
-					);
-				});
+				.then(() => {});
 		},
 	});
 	return <div></div>;
